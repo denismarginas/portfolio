4ab7a4941d3ec7b19f57fa18b84a7359c9d44db6
Denis Marginas <132045836+denismarginas@users.noreply.github.com>
2023-06-07 15:10:45 +0300

    Page: Home - in progress
    - changes categories section

--- DM - Personal Portfolio/themes/dm-theme/assets/js/theme_script.js
+++ DM - Personal Portfolio/themes/dm-theme/assets/js/theme_script.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var navbarToggle = document.querySelector(".dm-navbar-toggle");
     var navbarToggleSection = document.querySelector(".dm-menu");
 
-    navbarToggle.addEventListener("click", function() {
-      navbarToggle.classList.toggle("active");
-      navbarToggleSection.classList.toggle("navbar-active");
-    });
+    if(navbarToggle && navbarToggleSection){
+        navbarToggle.addEventListener("click", function() {
+            navbarToggle.classList.toggle("active");
+            navbarToggleSection.classList.toggle("navbar-active");
+        });
+    }
 });
   
