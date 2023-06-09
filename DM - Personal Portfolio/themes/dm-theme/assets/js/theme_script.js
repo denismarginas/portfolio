ffa373ebb168de0348bfa06ed2ab3148039596e5
Denis Marginas <denismarginas09@gmail.com>
2023-06-09 12:09:03 +0300

    Page: Home - alpha
    - Changes Section Web Development Description

--- DM - Personal Portfolio/themes/dm-theme/assets/js/theme_script.js
+++ DM - Personal Portfolio/themes/dm-theme/assets/js/theme_script.js
@@ -1,5 +1,6 @@
-// Navbar Function
+// Page Loaded
 document.addEventListener("DOMContentLoaded", function() {
+    // Navbar Function
     var navbarToggle = document.querySelector(".dm-navbar-toggle");
     var navbarToggleSection = document.querySelector(".dm-menu");
 
@@ -9,5 +10,36 @@ document.addEventListener("DOMContentLoaded", function() {
             navbarToggleSection.classList.toggle("navbar-active");
         });
     }
+
+// Image Absolute Scroll Animation Bottom
+    function scrollAnimation(element, minBottom, maxBottom) {
+        var scrollPosition = window.scrollY || window.pageYOffset;
+        var bottomValue = Math.max(Math.min(-scrollPosition, maxBottom), minBottom);
+        element.style.bottom = bottomValue + "px";
+    }
+
+    window.onload = function() {
+        var scrollElements = document.querySelectorAll('[data-animation="dm-scroll"]');
+        if (scrollElements.length > 0) {
+            window.addEventListener("scroll", function() {
+                scrollElements.forEach(function(element) {
+                    if (element.classList.contains('dm-tablet-responsive')) {
+                        scrollAnimation(element, -10, 10);
+                    } else if (element.classList.contains('dm-phone-responsive')) {
+                        scrollAnimation(element, -20, 7);
+                    } else {
+                        scrollAnimation(element, -300, 0);
+                    }
+                });
+            });s
+        }
+    };
+
+
+
+
+
+
+
 });
   
