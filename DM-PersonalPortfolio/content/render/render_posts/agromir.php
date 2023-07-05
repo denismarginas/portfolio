aa3fedb10c8a95e2140520af163fbf605ec17f95
Denis Marginas <132045836+denismarginas@users.noreply.github.com>
2023-07-05 13:02:22 +0300

    Posts Add

--- DM-PersonalPortfolio/content/render/render_posts/agromir.php
+++ DM-PersonalPortfolio/content/render/render_posts/agromir.php
@@ -6,13 +6,14 @@ $renderer_sections = new RendererSections();
 $renderer_structure->header();
 
 $post_data = [
+    "visibility" => "enable",
     "post_type" => "portfolio",
     "media_path" => "agromir",
     "title" => "Agromir",
     "logo" => "logo/logo.png",
     "logo_type" => "png",
     "description" => "The online shop for tools, industrial equipment tires, tractor tires is made in Prestashop 1.7. The products cannot be added to the cart, but there are 3 buttons to contact by phone for the product and 1 button to contact by email. The contact form was created by editing TPL, HTML, CSS, and an iframe with a WordPress page with the Contact Form 7 module. The site has implemented modules from Prestashop Addons such as JoliSearch, Amazing gallery and other modules purchased or adapted free of charge for the site. A button is displayed to contact via Whatsapp for products through a module that has been modified via JS to display various texts at a certain time.",
-    "category" => [
+    "categories" => [
                     "Web Development Projects",
                     "Visual Media Projects"
                   ],
@@ -22,24 +23,26 @@ $post_data = [
     "media_facebook_url" => "https://www.facebook.com/cauciucuriagricole",
     "media_instagram_url" => "https://www.instagram.com/agromir_insta/",
     "employ" => "Pia Soft Product",
-    "date" => "2020-2022",
+    "date" => "02.2020-11.2022",
     "tags" => [
                 "web",
+                "media-web",
                 "photo",
                 "video",
-              ],
+    ],
     "colors" => [
-        "post_color_primary" => "#72b334",
-        "post_color_secondary" => "#FFFFFF",
-        "post_color_background" => "#183243",
-        "post_color_text_on_background" => "#FFFFFF"
+                "post_color_primary" => "#72b334",
+                "post_color_secondary" => "#FFFFFF",
+                "post_color_background" => "#183243",
+                "post_color_text_on_background" => "#FFFFFF"
     ]
 ];
 $post_content = "";
-$post_content .= "<h2 id='webdevelopmentprojects' class='dm-post-title-category'>Web Development Projects</h2>";
+$post_content .= "<h2 id='webdevelopmentprojects' class='dm-post-title-category' data-motion='transition-fade-0 transition-slideInRight-0' data-duration='0.7s'>Web Development Projects</h2>";
 $post_content .= renderGalleryWeb($post_data);
-
-$post_content .= "<h2 id='visualmediaprojects' class='dm-post-title-category'>Visual Media Projects</h2>";
+$post_content .= "<h2 id='webdevelopmentmedia' class='dm-post-title-category' data-motion='transition-fade-0 transition-slideInRight-0' data-duration='0.7s'>Web Media Content</h2>";
+$post_content .= renderGalleryWebMedia($post_data);
+$post_content .= "<h2 id='visualmediaprojects' class='dm-post-title-category' data-motion='transition-fade-0 transition-slideInRight-0' data-duration='0.7s'>Visual Media Projects</h2>";
 $post_content .= renderGalleryMedia($post_data);
 $post_content .= renderVideoMedia($post_data);
 
---
+++ DM-PersonalPortfolio/content/render/render_posts/piasoftproduct.php
@@ -0,0 +1,56 @@
+<?php
+$renderer_structure = new RendererStructure();
+$renderer_sections = new RendererSections();
+
+// Function Header
+$renderer_structure->header();
+
+$post_data = [
+    "visibility" => "enable",
+    "post_type" => "portfolio",
+    "media_path" => "piasoftproduct",
+    "title" => "Pia Soft Product",
+    "logo" => "logo/logo.png",
+    "logo_type" => "png",
+    "description" => "This is a company's presentation site, and is made in Wordpress with modules such as Elementor and Contact Form 7. Also the redesign logo was made by me to. The photo elements was edited in Photopea. At this website I didn't make all the pages, and all the SEO optimization. Here I work with colleague on this websites and on many more. I was hired first time at this small company in 2019 October, we were 2 workers at the beginning only, but the only part where we work together was just on SEO optimization and on the error checking at websites. After approximate 1 year and half from the point I worked there, only me remained as an employee. In total I worked 3 years and 1 month at this company over 30 websites in Wordpress and Prestashop, and over 2000 foto-video media for promotions.",
+    "categories" => [
+        "Web Development Projects",
+        "Visual Media Projects"
+    ],
+    "website_url" => "www.piasoftproduct.com",
+    "website_platform" => "Wordpress",
+    "website_status" => "Done",
+    "media_facebook_url" => "www.facebook.com/piasoftproduct",
+    "media_instagram_url" => "www.instagram.com/piasoftproduct/",
+    "employ" => "Pia Soft Product",
+    "date" => "10.2019-11.2022",
+    "tags" => [
+                "web",
+                "media-web",
+                "photo",
+                "video",
+    ],
+    "colors" => [
+        "post_color_primary" => "#ffcc00",
+        "post_color_secondary" => "#212121",
+        "post_color_background" => "#212121",
+        "post_color_text_on_background" => "#FFFFFF"
+    ]
+];
+
+$post_content = "";
+$post_content .= "<h2 id='webdevelopmentprojects' class='dm-post-title-category' data-motion='transition-fade-0 transition-slideInRight-0' data-duration='0.7s'>Web Development Projects</h2>";
+$post_content .= renderGalleryWeb($post_data);
+
+$post_content .= "<h2 id='visualmediaprojects' class='dm-post-title-category' data-motion='transition-fade-0 transition-slideInRight-0' data-duration='0.7s'>Web Media Content</h2>";
+$post_content .= renderGalleryMedia($post_data);
+$post_content .= renderVideoMedia($post_data);
+
+
+$renderer_sections->renderSection('post', "wrapper-layout", [$post_data, $post_content]);
+
+
+// Function Footer
+$renderer_structure->footer();
+
+?>
