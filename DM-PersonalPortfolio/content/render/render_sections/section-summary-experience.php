dd90f08cb8509f24b1a8cd978b6512a5bf99ff7a
Denis Marginas <denismarginas09@gmail.com>
2023-07-29 17:09:44 +0300

    Page: Summary experience - in progress 2

--- DM-PersonalPortfolio/content/render/render_sections/section-summary-experience.php
+++ DM-PersonalPortfolio/content/render/render_sections/section-summary-experience.php
@@ -1,12 +1,12 @@
 
 
 
-<section class="dm-summary-experience grid-background-animation"
-    <container>
-        <div class="dm-experience-header" data-motion="transition-fade-0 transition-slideInRight-0" data-duration="1s">
-            <h2>Web Development Projects</h2>
-        </div>
-        <div class="dm-experience-section" data-motion="transition-fade-0 transition-slideInRight-0" data-duration="1s">
+<section class="dm-summary-experience grid-background-animation">
+    <div class="dm-experience-header" data-motion="transition-fade-0 transition-slideInRight-0" data-duration="1s">
+        <h2>Web Development Projects</h2>
+    </div>
+    <div class="dm-experience-section" data-motion="transition-fade-0 transition-slideInRight-0" data-duration="1s">
+        <container>
             <ul>
                 <li>
                     <h3>PHP/SQL/HTML/JS/CSS FORM - WORDPRESS</h3>
@@ -49,38 +49,13 @@
             </ul>
             <ul>
                 <li>
-                    <div class="slideshow">
-                        <div class="slideshow-container">
-                            <div class="mySlides fade">
-                                <div class="numbertext">1 / 3</div>
-                                <?php echo renderImage($GLOBALS['urlPath']."content/img/summary-experience/promotions-contact-form-web-m-1.webp", true);?>
-                            </div>
 
-                            <div class="mySlides fade">
-                                <div class="numbertext">2 / 3</div>
-                                <?php echo renderImage($GLOBALS['urlPath']."content/img/summary-experience/promotions-con   tact-form-web-m-2.webp", true);?>
-                            </div>
-
-                            <div class="mySlides fade">
-                                <div class="numbertext">3 / 3</div>
-                                <?php echo renderImage($GLOBALS['urlPath']."content/img/summary-experience/promotions-contact-form-web-m-3.webp", true);?>
-                            </div>
-
-                            <a class="prev" onclick="plusSlides(-1)">❮</a>
-                            <a class="next" onclick="plusSlides(1)">❯</a>
-                        </div>
-                        <div style="text-align:center">
-                            <span class="dot" onclick="currentSlide(1)"></span>
-                            <span class="dot" onclick="currentSlide(2)"></span>
-                            <span class="dot" onclick="currentSlide(3)"></span>
-                        </div>
-                    </div>
                 </li>
                 <li>
                     <p>If the conditions are not met, a specific error message will be displayed below each field, and if the invoice code was previously used in the database, a pop-up message with a specific error message will be displayed or a banner will be displayed. If the campaign has started, the submit button from the form will not appear or a banner will appear instead of the form, if the campaign is over, a banner will appear instead of the form. A button to download CSV data from data base will show if you are an admin or editor of WordPress, or if you have the credentials from code inserted in URL. The form is implemented with PHP Everywhere widget and Elementor page builder.</p>
                 </li>
             </ul>
-        </div>
-    </container>
+        </container>
+    </div>
 </section>
 
