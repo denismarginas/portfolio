a1eac4b8e2ec48184ddca04e00a523379f155ddb
Denis Marginas <132045836+denismarginas@users.noreply.github.com>
2024-09-16 14:21:45 +0300

    404 page- dynamic text

--- content/render/render_sections/section-404.php
+++ content/render/render_sections/section-404.php
@@ -1,7 +1,27 @@
+<?php
+if(!isset($jsonNotFound)) :
+    $jsonNotFound = getDataJson('data-content-personal', 'data')["page-not-found"];
+endif;
+?>
+
 <section class="dm-404 grid-background-animation">
     <container>
-        <div class="title">404</div>
-        <p class="description">NOT FOUND</p>
+
+        <div class="title">
+            <?php if(isset($jsonNotFound["title"])) :
+                echo $jsonNotFound["title"];
+            else :
+                echo "404";
+            endif; ?>
+        </div>
+        <p class="description">
+
+            <?php if(isset($jsonNotFound["title"])) :
+                echo $jsonNotFound["description"];
+            else :
+                echo "NOT FOUND";
+            endif; ?>
+        </p>
     </container>
 </section>
 
