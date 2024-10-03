f1c2edf54202b1c592bce85da78aa09b9f127e4b
Denis Marginas <132045836+denismarginas@users.noreply.github.com>
2024-10-04 00:05:54 +0300

    Work Experience Section Update

--- content/render/render_sections/section-work-experience.php
+++ content/render/render_sections/section-work-experience.php
@@ -16,7 +16,7 @@ endif;
         <?php if( isset($jsonDataWorkExperience) and !empty($jsonDataWorkExperience)) : ?>
 
             <ul>
-                <li>
+                <li class="text">
                     <h2 data-motion="transition-fade-0 transition-slideInRight-0" data-duration="0.3s" data-delay="0s">
 
                         <?php if( isset($jsonDataWorkExperience["title"]) ): ?>
@@ -48,11 +48,34 @@ endif;
                     <?php endif; ?>
 
                 </li>
-                <li data-motion="transition-fade-0 transition-slideInLeft-0" data-duration="1s" data-delay="0.1s">
-                    <?php if( isset($jsonDataWorkExperience["img-path"]) && isset($jsonDataWorkExperience["img-preview"]) ):
-                        $img_preview = $GLOBALS['urlPath']."content/img/".$jsonDataWorkExperience["img-path"]."/".$jsonDataWorkExperience["img-preview"];
-                        echo renderImage($img_preview);
+                <li class="projects-previews" data-motion="transition-fade-0 transition-slideInLeft-0" data-duration="1s" data-delay="0s">
+                    <?php if( isset($jsonDataWorkExperience["images"]) && !empty($jsonDataWorkExperience["images"]) ):
+                        $images = $jsonDataWorkExperience["images"];
+                        $screen_img = $images["screen"];
+                        $cards_list_img = $images["cards"];
                     endif; ?>
+
+                    <?php if( isset($cards_list_img) && !empty($cards_list_img) ): ?>
+                        <div class="cards-container">
+                            <?php $nr = 1; $delay_card = 0.30; foreach ($cards_list_img as $card) :
+                                if( isset($card["img-path"]) && isset($card["img-preview"]) ):
+                                    $img_card = $GLOBALS['urlPath']."content/img/".$card["img-path"]."/".$card["img-preview"];
+                                    echo renderImage($img_card, false, "card-".$nr, true, ["data-motion" => "transition-fade-0 transition-slideInLeft-0", "data-duration" => "1s", "data-delay" => $nr*$delay_card."s"]);
+                                endif;
+                            $nr++; endforeach;
+                            ?>
+                            <?php if($nr > 2) : ?>
+                                <div class="bg" data-motion="transition-fade-0 transition-slideInLeft-0" data-duration="1.2s" data-delay="<?php echo $nr*$delay_card +0.1; ?>s"></div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if( isset($screen_img["img-path"]) && isset($screen_img["img-preview"]) ): ?>
+                        <div class="screen-container" data-motion="transition-fade-0 transition-slideInLeft-0" data-duration="1.2s" data-delay="0.1s">
+                            <?php $img_preview = $GLOBALS['urlPath']."content/img/".$screen_img["img-path"]."/".$screen_img["img-preview"];
+                            echo renderImage($img_preview); ?>
+                        </div>
+                    <?php endif; ?>
                 </li>
             </ul>
 
